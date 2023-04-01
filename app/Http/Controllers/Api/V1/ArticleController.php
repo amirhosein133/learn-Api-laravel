d2e3ea4c2ca1d693c5da5c0c0185e3810e42ff13
FALAHATI <amirhosein.xaanko@gmail.com>
2023-04-01 17:53:39 +0430

    put in github

---
+++ app/Http/Controllers/Api/V1/ArticleController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ArticleCollection;
+use App\Http\Resources\ArticleResource;
+use App\Models\Article;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $articles = Article::all();
+        return \response()->json(new ArticleCollection($articles), 200);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $this->validation($request);
+        Article::create([
+            'user_id' => 1,
+            'title' => $request->title,
+            'description' => $request->description,
+            'image' => $request->image
+        ]);
+        return \response()->json([
+            'message' => 'created'
+        ],201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Article $article)
+    {
+        return \response()->json([
+            'data' => new ArticleResource($article)
+        ], 200);
+        //handler error in Handler.php
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Article $article)
+    {
+        $article->update($request->all());
+        return \response()->json([
+           'message' => 'updated'
+        ],200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Article $article)
+    {
+        //
+    }
+
+    private function validation(Request $request)
+    {
+        $request->validate([
+            'title' => ['required'],
+            'description' => ['required']
+        ]);
+    }
+
+    private function imageUpload(Request $request)
+    {
+        return $request->hasFile('image')
+            ? $request->image->store('public') : null;
+    }
+}
